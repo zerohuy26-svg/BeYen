77c424ef1235ddcdd3d49da886c69c65e9fe6250
zerohuy26-svg <zerohuy26@gmail.com>
2025-12-04 01:15:12 +0700

    Add project files.

---
+++ WebApplication3/Program.cs
@@ -0,0 +1,7 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles();   // Quan trọng: cho phép dùng index.html làm trang mặc định
+app.UseStaticFiles();    // Cho phép truy cập file HTML/CSS/JS trong wwwroot
+
+app.Run();
